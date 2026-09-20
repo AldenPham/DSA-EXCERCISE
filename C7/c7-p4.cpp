4a9c8c0310f1e5cfe24c2af75d8c7f168f6b9294
PhamThanhTin1 <kawaiiidiein2007@gmail.com>
2026-09-20 10:19:31 +0700

    c7-p4 doned

---
+++ C7/c7-p4.cpp
@@ -0,0 +1,271 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+struct node{
+    public:
+    node* next;
+    int val;
+
+    node(int val){
+        next = nullptr;
+        this->val = val;
+    }
+};
+
+class deque
+{
+    private:
+    node* head = nullptr;
+    node* tail = nullptr;
+    int size = 0;
+    public:
+    void push_Front(int id){
+        node* curr = new node(id);
+
+        if(head == nullptr){
+            head = curr;
+            tail = curr;
+            size++;
+        }
+        else{
+            curr->next = head;
+            head = curr;
+            size++;
+        }
+    }
+    
+    void push_Back(int id){
+        node* curr = new node(id);
+        if(head == nullptr){
+            head = curr;
+            tail = curr;
+        }
+        else{
+            tail->next = curr;
+            tail = curr;
+        }
+
+        size++;
+    }
+
+    void push_Front_Node(node* target){
+        if(target == nullptr){
+            return;
+        }
+        else if(target == head && target == tail){
+            return;
+        }
+
+        //Tim node truoc target
+        node* curr = head;
+        while(curr->next != target && curr->next != nullptr){
+            curr = curr->next;
+        }
+
+        //Cat target
+        curr->next = target->next;
+        target->next = head;
+        head = target;
+    }
+
+    void push_Back_Node(node* target){
+        if(target == nullptr){
+            return;
+        }
+        else if(target == head && target == tail){
+            return;
+        }
+
+        //Tim node truoc target
+        node* curr = head;
+        while(curr->next != target && curr->next != nullptr){
+            curr = curr->next;
+        }
+
+        //Cat target
+        curr->next = target->next;
+        target->next = nullptr;
+        tail->next = target;
+        tail = target;
+    }
+
+    void pop_Front(){
+        if(head == nullptr){
+            return;
+        }
+        else if(head == tail){
+            node* p = head;
+            head = nullptr;
+            tail = nullptr;
+            delete p;
+            size--;   
+        }
+        else{
+            node* p = head;
+            head = head->next;
+            delete(p);
+            size--;
+        }
+    }
+
+    void pop_Back(){
+        if(head == nullptr){
+            return;
+        }
+        else if(head == tail){
+            node* p = tail;
+            head = nullptr;
+            tail = nullptr;
+            delete p;
+            size--;   
+        }
+        else{
+            //Tim node truoc target
+            node* curr = head;
+            while(curr->next != tail && curr->next != nullptr){
+                curr = curr->next;
+            }
+            curr->next = nullptr;
+            delete tail;
+            tail = curr;
+            size--;
+        }
+    }
+
+    void delete_Value(int id){
+        node* curr = head;
+        
+        if(head == nullptr){
+            return;
+        }
+
+        if(head->val == id){
+            pop_Front();
+            return;
+        }
+
+        while(curr->next != nullptr){
+            if(curr->next->val == id){
+                node* p = curr->next;
+                
+                curr->next = curr->next->next;
+                        
+                if(p == tail){
+                    tail = curr;
+                }
+
+                delete(p);
+                size--;
+                return;
+            }        
+
+            curr = curr->next;
+        }
+         
+    }
+
+    void insert_At_Node(node* target, node* insert) /*Insert node ngay sau target*/{
+        if(target == nullptr || insert == nullptr){
+            return;
+        }
+        else if(target == tail){
+            tail->next = insert;
+            tail = insert;
+        }
+        else if(insert == tail){
+            //Tim node truoc đó
+            node* curr = head;
+            while(curr->next != insert && curr->next != nullptr){
+                curr = curr->next;
+            }
+            curr->next = nullptr;
+            tail = curr;
+            insert->next = target->next;
+            target->next = insert;
+        }
+        else{
+            insert->next = target->next;
+            target->next = insert;
+        }
+        
+    }
+
+    int get_Size(){
+        return size;
+    }
+    
+    node* get_Head(){
+        return head;
+    }
+
+    node* get_Tail(){
+        return tail;
+    }
+};
+
+void run_Command(string command, deque &prioList){
+    string action;
+    int val;
+
+    stringstream ss(command);
+    // action ""
+    ss >> action >> val;
+
+    if(action == "PUSH_FRONT"){
+        prioList.push_Front(val);
+    }
+    else if(action == "PUSH_BACK"){
+        prioList.push_Back(val);
+    }
+    else if(action == "POP_FRONT"){
+        if(prioList.get_Head() == nullptr){
+            cout << "-1\n";
+        }
+        else{
+            cout << prioList.get_Head()->val << "\n";
+            prioList.pop_Front();
+        }
+        
+    }
+    else if(action == "POP_BACK"){
+        if(prioList.get_Tail() == nullptr){
+            cout << "-1\n";
+        }
+        else{
+            cout << prioList.get_Tail()->val << "\n";
+            prioList.pop_Back();
+        }
+    }
+    else if(action == "GET_FRONT"){
+        if(prioList.get_Head() != nullptr){
+            cout << prioList.get_Head()->val << "\n";
+        }
+        else{
+            cout << "-1\n";
+        }
+    }
+    else if(action == "GET_BACK"){
+        if(prioList.get_Tail() != nullptr){
+            cout << prioList.get_Tail()->val << "\n";
+        }
+        else{
+            cout << "-1\n";
+        }
+    }
+}
+
+int main(){
+    int Q;
+    cin >> Q;
+
+    cin.ignore();
+    string cmd;
+    deque prioList;
+    for(int i = 0; i < Q; i++){
+        getline(cin, cmd);
+        run_Command(cmd, prioList); 
+    }
+}
